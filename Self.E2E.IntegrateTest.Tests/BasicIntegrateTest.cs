229ef13e7853288c9b5fcc22a03ff6133e165bbb
Tony Lee <tony_lee@mayohr.com>
2022-11-05 19:26:39 +0800

    [feat] add basic integrate test

---
+++ Self.E2E.IntegrateTest.Tests/BasicIntegrateTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Self.E2E.IntegrateTest.Tests;
+
+[TestFixture]
+public class BasicIntegrateTest
+{
+    private WebApplicationFactory<Program> _factory;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _factory = new WebApplicationFactory<Program>();
+    }
+
+    [Test]
+    public async Task True_Send_ReturnCorrect()
+    {
+        var client = _factory.CreateClient();
+
+        var resp = await client.GetAsync("WeatherForecast/True");
+
+        resp.EnsureSuccessStatusCode();
+        var result = await resp.Content.ReadAsStringAsync();
+        
+        resp.Should().BeSuccessful();
+        result.Should().BeEquivalentTo("true");
+    }
+}
