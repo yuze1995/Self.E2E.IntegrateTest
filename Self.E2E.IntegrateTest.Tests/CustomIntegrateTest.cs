a8f7457908198a9870a41a918195fbd9e2548359
Tony Lee <tony_lee@mayohr.com>
2022-11-05 21:08:05 +0800

    [test] invoke custom webApplicationFactory

---
+++ Self.E2E.IntegrateTest.Tests/CustomIntegrateTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Self.E2E.IntegrateTest.Tests;
+
+public class CustomIntegrateTest
+{
+    private WebApplicationFactory<Program> _factory;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _factory = new CustomWebApplicationFactory();
+    }
+    
+    [Test]
+    public async Task Version_Send_ReturnV2()
+    {
+        var client = _factory.CreateClient();
+
+        var resp = await client.GetAsync("WeatherForecast/Version");
+
+        resp.EnsureSuccessStatusCode();
+        var result = await resp.Content.ReadAsStringAsync();
+        
+        resp.Should().BeSuccessful();
+        result.Should().BeEquivalentTo("V2");
+    } 
+}
---
+++ Self.E2E.IntegrateTest.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Version = Self.E2E.IntegrateTest.Service.Version;
+
+namespace Self.E2E.IntegrateTest.Tests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.Configure<Version>(option => option.Number = 2);
+        });
+    }
+}
